144576b65184580d8dbcb7deb36e48a5038ad47d
Jordan Capital <118566815+JordanCapital@users.noreply.github.com>
2023-02-20 00:18:08 +0300

    Update 3-music-collection.js
    object and function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,30 @@
 console.log('***** Music Collection *****')
+
+// Empty variable collection
+
+const collection = [];
+
+// function addToCollection, and object
+
+function addToCollection(title, artist, yearPublished) {
+    const album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    collection.push(album);
+
+    return album;
+}
+
+// Adding albums to addToCollection function
+addToCollection("Renaissance", "Beyonce", "2022");
+console.log("New album:", addToCollection("Teflon Don", "Rick ross", "2010"));
+console.log("New album:", addToCollection("Islah", "Kevin Gates", "2016"));
+console.log("New album:", addToCollection("Magic", "Nas", "2021"));
+console.log("New album:", addToCollection("Tha Carter III", "Lil Wayne", "2008"));
+console.log("New album:", addToCollection("Sorry 4 the wait", "Lil Wayne", "2011"));
+
+// console collection array
+
+console.log("collection:", collection);
